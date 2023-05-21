c4e0dcbe70fb31b7afe66293f60ef88a8a1e6f99
Pablo Arenivar <pabloarenivar@Pablos-MacBook-Pro.local>
2023-05-21 14:50:16 -0500

    first commit

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className='container flex flex-col items-center space-y-4'>
+            <h1 className='text-3xl font-semibold'>Error 404</h1>
+            <h2 className='text-2xl font-medium'>The page you&apos;re looking for doesn&apos;t exist.</h2>
+        </main>
+    )
+}
