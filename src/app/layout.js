9795bce528da990a3c122dddb7e2194e7791f02b
Pablo Arenivar <pabloarenivar@Pablos-MacBook-Pro.local>
2023-06-03 16:59:49 -0500

    Implemented Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { poppins } from '@/styles/fonts'
 
 import Footer from '@/components/global/Footer'
 import Nav from '@/components/global/Nav'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 export const metadata = {
   title: 'Bears Digital | Helping Businesses Grow through Creative Digital Design and Development Solutions.',
@@ -12,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleAnalytics GA_MEASUREMENT_ID='' />
       <body className={`flex flex-col items-center ${poppins.className}`}>
         <Nav />
         {children}
